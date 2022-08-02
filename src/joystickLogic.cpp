8d3c2a1aca36c0d34fd319b65765258d18509743
vortex314 <lieven.merckx@gmail.com>
2022-08-02 20:04:32 +0200

    upd and reconnect

---
+++ src/joystickLogic.cpp
@@ -0,0 +1,33 @@
+#include <Redis.h>
+#include <limero.h>
+#include <stdint.h>
+
+extern Log logger;
+
+template <typename T>
+T scale(T x, T x1, T x2, T y1, T y2) {
+  return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
+}
+
+void joystickLogic(Redis &redis, Thread &workerThread) {
+  TimerSource *timerWatchdog =
+      new TimerSource(workerThread, 1000, true, "ticker");
+
+  auto &pubWatchdog = redis.publisher<bool>("dst/hover/motor/watchdogReset");
+
+  *timerWatchdog >> [&](const TimerMsg &) { pubWatchdog.on(true); };
+
+  redis.subscriber<int32_t>("src/joystick/axis/3") >>
+      *new LambdaFlow<int32_t, int32_t>([&](int32_t &out, const int32_t &in) {
+        out = -scale(in, -32768, +32768, -1000, +1000);
+        return true;
+      }) >>
+      redis.publisher<int32_t>("dst/hover/motor/speedTarget");
+
+  redis.subscriber<int32_t>("src/joystick/axis/2") >>
+      *new LambdaFlow<int32_t, int32_t>([&](int32_t &out, const int32_t &in) {
+        out = scale(in, -32768, +32768, -1000, +1000);
+        return true;
+      }) >>
+      redis.publisher<int32_t>("dst/hover/motor/steerTarget");
+}
--- src/redisBrain.cpp
+++ src/redisBrain.cpp
@@ -1,4 +1,4 @@
-#include <Config.h>
+#include <ConfigFile.h>
 #include <Hardware.h>
 #include <Log.h>
 #include <limero.h>
@@ -12,11 +12,16 @@ std::string loadFile(const char *name);
 bool loadConfig(JsonObject cfg, int argc, char **argv);
 int timeOfDay();
 
+extern void joystickLogic(Redis&,Thread&);
+
 int main(int argc, char **argv) {
-  INFO("Start %s", argv[0]);
-  DynamicJsonDocument config(10240);
-  loadConfig(config.to<JsonObject>(), argc, argv);
+  INFO("Loading configuration.");
+  Json config;
+  config["redis"]["host"] = "localhost";
+  config["redis"]["port"] = 6379;
+  configurator(config, argc, argv);
   Thread workerThread("worker");
+
   Redis redis(workerThread, config["redis"].as<JsonObject>());
 
   redis.connect();
@@ -31,6 +36,8 @@ int main(int argc, char **argv) {
     serializeJson(json, s);
     //  INFO("Got response : %s", s.c_str());
   };
+
+  joystickLogic(redis,workerThread);
 
   TimerSource ticker(workerThread, 300000, true, "ticker");
   ValueFlow<bool> shake;
@@ -68,41 +75,3 @@ int timeOfDay() {
   info = localtime(&t);
   return (info->tm_hour * 100) + info->tm_min;
 }
-
-void deepMerge(JsonVariant dst, JsonVariant src) {
-  if (src.is<JsonObject>()) {
-    for (auto kvp : src.as<JsonObject>()) {
-      deepMerge(dst.getOrAddMember(kvp.key()), kvp.value());
-    }
-  } else {
-    dst.set(src);
-  }
-}
-
-bool loadConfig(JsonObject cfg, int argc, char **argv) {
-  // override args
-  int c;
-  while ((c = getopt(argc, argv, "f:v")) != -1) switch (c) {
-      case 'f': {
-        std::string s = loadFile(optarg);
-        DynamicJsonDocument doc(10240);
-        deserializeJson(doc, s);
-        deepMerge(cfg, doc);
-        break;
-      }
-      case 'v': {
-        logger.setLevel(Log::L_DEBUG);
-        break;
-      }
-      case '?':
-        printf("Usage %s -v -f <configFile.json>", argv[0]);
-        break;
-      default:
-        WARN("Usage %s -v -f <configFile.json> ", argv[0]);
-        abort();
-    }
-  std::string s;
-  serializeJson(cfg, s);
-  INFO("config:%s", s.c_str());
-  return true;
-};
