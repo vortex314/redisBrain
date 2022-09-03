f84cfa7c261ecbe8bedcb508d24b558ad7e57eea
vortex314 <lieven.merckx@gmail.com>
2022-09-03 23:14:09 +0200

    power on & off for 42V power

--- src/joystickLogic.cpp
+++ src/joystickLogic.cpp
@@ -5,33 +5,77 @@
 extern Log logger;
 
 template <typename T>
-T scale(T x, T x1, T x2, T y1, T y2) {
+T scale(T x, T x1, T x2, T y1, T y2)
+{
   return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
 }
 
-void joystickLogic(Redis &redis, Thread &workerThread) {
+void joystickLogic(Redis &redis, Thread &workerThread)
+{
   TimerSource *timerWatchdog =
       new TimerSource(workerThread, 1000, true, "ticker");
 
   auto &pubWatchdog = redis.publisher<bool>("dst/hover/motor/watchdogReset");
   auto &redisBrainAlive = redis.publisher<bool>("src/redisBrain/system/alive");
 
-  *timerWatchdog >> [&](const TimerMsg &) {
+  *timerWatchdog >> [&](const TimerMsg &)
+  {
     pubWatchdog.on(true);
     redisBrainAlive.on(true);
   };
 
   redis.subscriber<int32_t>("src/joystick/axis/3") >>
-      *new LambdaFlow<int32_t, int32_t>([&](int32_t &out, const int32_t &in) {
+      *new LambdaFlow<int32_t, int32_t>([&](int32_t &out, const int32_t &in)
+                                        {
         out = -scale(in, -32768, +32768, -1000, +1000);
-        return true;
-      }) >>
+        return true; }) >>
       redis.publisher<int32_t>("dst/hover/motor/speedTarget");
 
   redis.subscriber<int32_t>("src/joystick/axis/2") >>
-      *new LambdaFlow<int32_t, int32_t>([&](int32_t &out, const int32_t &in) {
+      *new LambdaFlow<int32_t, int32_t>([&](int32_t &out, const int32_t &in)
+                                        {
         out = scale(-in, -32768, +32768, -90, +90);
-        return true;
-      }) >>
+        return true; }) >>
       redis.publisher<int32_t>("dst/hover/motor/angleTarget");
+
+  redis.subscriber<int>("src/joystick/button/0") >>
+      *new LambdaFlow<int, int32_t>([&](int32_t &out, const int &in)
+                                     { INFO(
+                                           " button 0 : %s", in ? "true" : "false");
+                                           if (in) {
+                                             out = 0;
+                                             return true;
+                                           } return false; }) >>
+      redis.publisher<int32_t>("dst/raspi/gpio9/value");
+
+  redis.subscriber<int>("src/joystick/button/0") >>
+      *new LambdaFlow<int, int32_t>([&](int32_t &out, const int &in)
+                                    {
+        if ( in ) {
+          out=0;
+          return true;
+        }
+        return false; }) >>
+      redis.publisher<int32_t>("dst/raspi/gpio15/value");
+
+  redis.subscriber<int>("src/joystick/button/1") >>
+      *new LambdaFlow<int, int32_t>([&](int32_t &out, const int &in)
+                                    {
+    if (in)
+    {
+      out = 1;
+      return true;
+    }
+    return false; }) >>
+      redis.publisher<int32_t>("dst/raspi/gpio9/value");
+
+  redis.subscriber<int>("src/joystick/button/1") >>
+      *new LambdaFlow<int, int32_t>([&](int32_t &out, const int &in)
+                                    {
+        if ( in ) {
+          out=1;
+          return true;
+                                     }
+        return false; }) >>
+      redis.publisher<int32_t>("dst/raspi/gpio15/value");
 }
