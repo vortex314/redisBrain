bcb91242796d85ea89284812627475c7e0626cfb
lieven <lieven.merckx@gmail.com>
2022-09-07 18:02:40 +0200

    readable logic ?

--- src/joystickLogic.cpp
+++ src/joystickLogic.cpp
@@ -15,6 +15,17 @@ LambdaFlow<IN, OUT> &flow(std::function<bool(OUT &out, const IN &in)> f) {
 }
 
 template <typename IN, typename OUT>
+LambdaFlow<IN, OUT> &timeout(Thread &thread, int to, OUT v) {
+  TimerSource &timer = *new TimerSource(thread, 1000, true, "timer");
+  auto &lf = *new LambdaFlow<IN, OUT>([&](OUT &out, const IN &in) {
+    timer.reset();
+    return false;
+  });
+  timer >> [&](const TimerMsg &tm) { lf.emit(v); };
+  return lf;
+}
+
+template <typename IN, typename OUT>
 LambdaFlow<IN, OUT> &map(IN v1, OUT v2) {
   return *new LambdaFlow<IN, OUT>([v1, v2](OUT &out, const IN &in) {
     if (in == v1) {
@@ -27,14 +38,6 @@ LambdaFlow<IN, OUT> &map(IN v1, OUT v2) {
 }
 
 template <typename IN, typename OUT>
-LambdaFlow<IN, OUT> &timeout(Thread &thread, int timeout_msec, OUT v1) {
-  TimerSource &ts = *new TimerSource(thread, timeout_msec, true);
-
-  return *new LambdaFlow<IN, OUT>(
-      [v1, ts](OUT &out, const IN &in) { return false; });
-}
-
-template <typename IN, typename OUT>
 LambdaFlow<IN, OUT> *range(IN inMultiplier, IN x1, IN x2, OUT outMultiplier,
                            OUT y1, OUT y2) {
   return new LambdaFlow<IN, OUT>([=](OUT &out, const IN &in) {
@@ -44,45 +47,52 @@ LambdaFlow<IN, OUT> *range(IN inMultiplier, IN x1, IN x2, OUT outMultiplier,
   });
 }
 void joystickLogic(Redis &r, Thread &workerThread) {
-  TimerSource *timerWatchdog =
-      new TimerSource(workerThread, 1000, true, "ticker");
+  // STATE
+  auto &power_on_state = *new ValueFlow<int>(0);
+  r.subscriber<int>("dst/limero/powerOn") >> power_on_state >>
+      r.publisher<int>("src/limero/powerOn");
+  // INPUT
+  auto &power_on_button = r.subscriber<int>("src/joystick/button/0");
+  auto &power_off_button = r.subscriber<int>("src/joystick/button/1");
+  auto &collison_left = r.subscriber<int>("src/sideboard/sensor/left");
+  auto &collison_right = r.subscriber<int>("src/sideboard/sensor/right");
+  auto &steer_joystick = r.subscriber<int>("src/joystick/axis/2");
+  auto &speed_joystick = r.subscriber<int>("src/joystick/axis/3");
+  auto &joystick_reader = r.subscriber<bool>("src/joystick/system/alive");
+
+  // OUTPUT
 
   auto &pubWatchdog = r.publisher<bool>("dst/hover/motor/watchdogReset");
   auto &redisBrainAlive = r.publisher<bool>("src/redisBrain/system/alive");
+  auto &hover_motor_speed = r.publisher<int>("dst/hover/motor/speedTarget");
+  auto &hover_motor_steer = r.publisher<int>("dst/hover/motor/angleTarget");
+  auto &power_relais_1 = r.publisher<int>("dst/raspi/gpio9/value");
+  auto &power_relais_2 = r.publisher<int>("dst/raspi/gpio15/value");
 
-  r.subscriber<int32_t>("src/joystick/axis/3") >>
-      range(1, -32768, +32768, -1, -1000, +1000) >>
-      r.publisher<int32_t>("dst/hover/motor/speedTarget");
+  TimerSource *timerWatchdog =
+      new TimerSource(workerThread, 1000, true, "ticker");
 
-  r.subscriber<int32_t>("src/joystick/axis/2") >>
-      range(-1, -32768, +32768, +1, -90, +90) >>
-      r.publisher<int32_t>("dst/hover/motor/angleTarget");
+  speed_joystick >> range(1, -32768, +32768, -1, -1000, +1000) >>
+      hover_motor_speed;
 
-  auto &button_0 = r.subscriber<int>("src/joystick/button/0");
-  auto &button_1 = r.subscriber<int>("src/joystick/button/1");
-  auto &joystickAlive = r.subscriber<bool>("src/joystick/system/alive")
+  steer_joystick >> range(-1, -32768, +32768, +1, -90, +90) >>
+      hover_motor_steer;
 
-                            auto &powerOn = *new ValueFlow<int>(0);
-  r.subscriber<int>("dst/limero/powerOn") >> powerOn >>
-      r.publisher<int>("src/limero/powerOn");
+  power_on_button >> map(1, 1) >> power_on_state;
+  power_off_button >> map(1, 0) >> power_on_state;
+  joystick_reader >> timeout<bool,int>(workerThread, 3000, 0) >> power_on_state;
+  collison_left >> map(1, 0) >> power_on_state;
+  collison_right >> map(1, 0) >> power_on_state;
 
-  joystickAlive >> timeout(0) >> powerOn;
-  button_0 >> map(1, 1) >> powerOn;
-  button_1 >> map(1, 0) >> powerOn;
-  powerOn >> map(1, 0) >> r.publisher<int>("dst/raspi/gpio9/value");
-  powerOn >> map(1, 0) >> r.publisher<int>("dst/raspi/gpio15/value");
-  powerOn >> map(0, 1) >> r.publisher<int>("dst/raspi/gpio9/value");
-  powerOn >> map(0, 1) >> r.publisher<int>("dst/raspi/gpio15/value");
-
-  r.subscriber<int>("src/sideboard/sensor/left") >> map(1, 0) >> powerOn;
-  r.subscriber<int>("src/sideboard/sensor/right") >> map(1, 0) >> powerOn;
-
-  auto &pubSrcPowerOn = r.publisher<int>("src/limero/powerOn");
+  power_on_state >> map(1, 0) >> power_relais_1;  // power on relais 1
+  power_on_state >> map(1, 0) >> power_relais_2;  // power on relais 2
+  power_on_state >> map(0, 1) >> power_relais_1;  // power off relais 1
+  power_on_state >> map(0, 1) >> power_relais_2;  // power off relais 2
 
   *timerWatchdog >> [&](const TimerMsg &) {
     pubWatchdog.on(true);
     redisBrainAlive.on(true);
-    powerOn.request();
+    power_on_state.request();
   };
   workerThread.run();
 }
