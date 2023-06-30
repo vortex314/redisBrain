c4e5f6d4f63b371f14c8829f3a4a32a674913e8b
lieven <lieven.merckx@gmail.com>
2023-07-01 00:14:46 +0200

    added alive publish

--- src/redisBrain.cpp
+++ src/redisBrain.cpp
@@ -41,26 +41,35 @@ int main(int argc, char **argv) {
   //  joystickLogic(redis, workerThread);
 
   auto &ticker = workerThread.createTimer(300000, true, true, "ticker");
+  auto &alive_ticker = workerThread.createTimer(1000, true, true, "alive_ticker");
 
-  ValueFlow<bool> shake(workerThread);
+  ValueFlow<int	> shake(workerThread);
   ;
-  shake >> redis.publisher<bool>("dst/shaker1/shake/trigger");
-  shake >> redis.publisher<bool>("dst/shaker2/shake/trigger");
-  shake >> redis.publisher<bool>("dst/shaker3/shake/trigger");
+  shake >> redis.publisher<int	>("dst/shaker1/shake/trigger");
+  shake >> redis.publisher<int	>("dst/shaker2/shake/trigger");
+  shake >> redis.publisher<int	>("dst/shaker3/shake/trigger");
   redis.subscriber<uint32_t>("dst/brain/shaker/interval") >>
       [&](const uint32_t &count) {
         INFO("Got interval %d", count);
         ticker.interval(count);
       };
 
+
   ticker >> [&shake](const TimerSource &) {
     int now = timeOfDay();
     if (now > 529 && now < 2200) {
       INFO("let's shake it %d ", now);
-      shake = true;
+      shake.on(1);
     } else {
       INFO(" sleeping.... ");
     }
+  };
+
+  auto &alive_publisher= redis.publisher<int>("src/redisBrain/sys/alive");
+
+  alive_ticker >> [&alive_publisher](const TimerSource&) {
+	  const int x=1;
+	  alive_publisher.on(x);
   };
 
   workerThread.run();
